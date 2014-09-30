eb61b1df84a3e07967b4db6dc80d4c23824d2013
Anton Taraev <anti191@gmail.com>
2014-09-30 15:21:17 +0400

    Fix golint notise

--- client/client.go
+++ client/client.go
@@ -14,7 +14,7 @@ import (
 )
 
 type Client struct {
-	Url   string
+	URL   string
 	Token string
 }
 
@@ -22,7 +22,7 @@ func (client *Client) request(method string, action string, o interface{}, param
 	var bodyReader io.Reader
 	var req *http.Request
 	var httpClient = &http.Client{}
-	uri := fmt.Sprintf("http://%s/backend/%s?token=%s", client.Url, action, client.Token)
+	uri := fmt.Sprintf("http://%s/backend/%s?token=%s", client.URL, action, client.Token)
 	if len(params) > 0 {
 		json, err := json.Marshal(params[0])
 		if err != nil {

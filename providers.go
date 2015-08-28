a2c051de357cbc7890c931051877d753aecc055a
Maxim Davydov <maximdavydoff@yandex.ru>
2015-08-28 12:32:27 +0300

    Add bucket to p command

--- providers.go
+++ providers.go
@@ -11,8 +11,8 @@ func providerAction(c *cli.Context) {
 	var providerConfig config.Provider
 	switch c.Command.Name {
 	case "AWS":
-		validateArgs(c, 2)
-		providerConfig = config.Provider{"AWS_ACCESS_KEY_ID": c.Args()[0], "AWS_SECRET_ACCESS_KEY": c.Args()[1]}
+		validateArgs(c, 3)
+		providerConfig = config.Provider{"bucket": c.Args()[0], "AWS_ACCESS_KEY_ID": c.Args()[1], "AWS_SECRET_ACCESS_KEY": c.Args()[2]}
 	case "encryption":
 		validateArgs(c, 1)
 		providerConfig = config.Provider{"pass": c.Args()[0]}
